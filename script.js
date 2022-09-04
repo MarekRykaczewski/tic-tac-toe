9bba06470eb37f4723972d72f67c7d95eb4277a5
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-09-04 17:04:14 +0200

    add gameBoard and displayController objects, add updateDisplay function

---
+++ script.js
@@ -0,0 +1,31 @@
+const gameBoard = (() => {
+    board = ['O', 'X', 'O',
+             'O', 'X', 'O',
+             'O', 'X', 'O']
+
+    return {
+        board: board
+    }
+})
+
+const displayController = (() => {
+    const updateDisplay = function(gameBoard) {
+        let boardContainer = document.querySelector("#board-container")
+        for (let i = 0; i < gameBoard.length; i++) {
+            let cell = document.createElement("div")
+            boardContainer.appendChild(cell)
+        }
+
+    }
+
+    return {
+        updateDisplay: updateDisplay
+    }
+})
+
+let newGameBoard = gameBoard()
+let newDisplayController = displayController()
+
+// console.log(newGameBoard.board)
+newDisplayController.updateDisplay(newGameBoard.board)
+
